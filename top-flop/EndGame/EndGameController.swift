89c59d4c059acf853db0d17deb2e017dfa7106e9
Ilia Rodionov <ilia.rodionov.mail@gmail.com>
2022-11-22 14:09:19 +0300

    (Жарков) [*] Закончил вёрстку экранов

---
+++ top-flop/EndGame/EndGameController.swift
@@ -0,0 +1,15 @@
+//
+//  EndGameController.swift
+//  top-flop
+//
+//  Created by Илья Родионов on 22.11.2022.
+//
+
+import UIKit
+
+class EndGameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
