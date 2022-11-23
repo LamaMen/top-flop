7dda93d64e58b68c60eb955b3075871a19557bcf
Ilia Rodionov <ilia.rodionov.mail@gmail.com>
2022-11-23 18:30:16 +0300

    (Жарков) [*] Сверстал два экрана

---
+++ top-flop/RoundEnd/RoundEndViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TutorialViewController.swift
+//  top-flop
+//
+//  Created by Илья Родионов on 23.11.2022.
+//
+
+import UIKit
+
+class RoundEndViewController: UIViewController {
+
+    @IBOutlet weak var time: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let s = "3:2"
+        time.text = s + "1"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
