db86c70cc3ca6f7b393fd2c1ac98bc5991631569
Ilia Rodionov <ilia.rodionov.mail@gmail.com>
2022-11-23 19:44:34 +0300

    (Сураев) [*] Отображается значение слайдера

--- top-flop/GameSetting/GameSettingController.swift
+++ top-flop/GameSetting/GameSettingController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class GameSettingController: UIViewController {
 
+    @IBOutlet weak var gamersCountSlider: UISlider!
+    @IBOutlet weak var lapTimeSlider: UISlider!
+    
+    
+    @IBOutlet weak var gamersCountLabel: UILabel!
+    @IBOutlet weak var lapTimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func gamersCountChange(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        gamersCountLabel.text = "\(currentValue)"
+    }
+    
+    @IBAction func lapTimeChange(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        lapTimeLabel.text = "\(currentValue)"
+    }
     /*
     // MARK: - Navigation
 
